32b7d439222ec65309737099aec65b31e8deed8f
Maxwell Krohn <themax@gmail.com>
2014-06-16 15:57:15 -0400

    a patching script to cut over to bn

---
+++ patch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Run this on fresh checkout for our patches...
+
+FIND="require('bigi')"
+REPLACE="require('bn').BigInteger"
+regex="s/${FIND}/${REPLACE}/"
+
+find test src -type f -regex '.*\.js' -exec sed -i.bak ${regex} {} \;
+find test src -type f -regex '.*\.js.bak' -exec rm {} \;
